91736f9fb4b602b16f80d6998e24b676db999383
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-11-08 16:18:54 +0100

    Cleaned up plots

--- project.py
+++ project.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 
     build_cpp()
     temp = 1
-    nmax = int(1e7)
+    nmax = int(1e6)
     L = 2
     file = rootdir + "/data/test.dat"
     run(["./main.exe", file, "single", f"{L}", f"{nmax}", f"{temp}"], cwd=src)
@@ -57,39 +57,43 @@ if __name__ == "__main__":
     print(f"{np.mean(M):e}")
     print(f"{np.mean(absM)-absM_exp:e}")
 
-    n_cycles = np.linspace(1, nmax-1, len(E))
+    n_cycles = np.linspace(1, nmax, len(E))
     E = np.cumsum(E)/n_cycles
-    M = np.cumsum(M)/n_cycles
+    # M = np.cumsum(M)/n_cycles
     absM = np.cumsum(absM)/n_cycles
 
     plt.figure()
-    plt.title(f"Average energy of {L}x{L} lattice")
+    plt.title(f"Average energy of {L}x{L} lattice with T = {temp}")
     plt.hlines(E_exp, 0, nmax, 'r', label="Analytic")
     plt.plot(n_cycles, E, label="<E>")
     plt.xlabel("N")
     plt.ylabel("<E>")
     plt.legend()
     plt.grid()
-    plt.savefig(rootdir + f"/data/{L}x{L}-E.pdf", bbox_inches='tight')
+    plt.savefig(
+        rootdir + f"/data/t{temp*10}-{L}x{L}-E.pdf", bbox_inches='tight')
+
+    # plt.figure()
+    # plt.title(f"Average magnetization of {L}x{L} lattice")
+    # plt.hlines(0, 0, nmax, 'r', label="Analytic")
+    # plt.plot(n_cycles, M, label="<M>")
+    # plt.xlabel("N")
+    # plt.ylabel("<M>")
+    # plt.legend()
+    # plt.grid()
+    # plt.savefig(
+    #     rootdir + f"/data/t{temp*10}-{L}x{L}-M.pdf", bbox_inches='tight')
 
     plt.figure()
-    plt.title(f"Average magnetization of {L}x{L} lattice")
-    plt.hlines(0, 0, nmax, 'r', label="Analytic")
-    plt.plot(n_cycles, M, label="<M>")
-    plt.xlabel("N")
-    plt.ylabel("<M>")
-    plt.legend()
-    plt.grid()
-    plt.savefig(rootdir + f"/data/{L}x{L}-M.pdf", bbox_inches='tight')
-
-    plt.figure()
-    plt.title(f"Average magnitude of magnetization of {L}x{L} lattice")
+    plt.title(f"Average magnitude of magnetization of {L}x{L}" +
+              f"lattice with T = {temp}")
     plt.hlines(absM_exp, 0, nmax, 'r', label="Analytic")
     plt.plot(n_cycles, absM, label="<|M|>")
     plt.xlabel("N")
     plt.ylabel("<|M|>")
     plt.legend()
     plt.grid()
-    plt.savefig(rootdir + f"/data/{L}x{L}-|M|.pdf", bbox_inches='tight')
+    plt.savefig(
+        rootdir + f"/data/t{temp*10}-{L}x{L}-|M|.pdf", bbox_inches='tight')
 
     plt.show()
