1d4a6476311f89af9ac13bf7bf2ad01b9d00b912
Anders Petershagen Åsbø <anderpaa@uio.no>
2020-11-10 10:46:06 +0100

    completed function for spawning concurrent simulations

--- project.py
+++ project.py
@@ -86,20 +86,21 @@ def stabilization_run(nmax, temp, L, randspin=False):
 
 
 def phase_trans_test(nmax):
+    # lists of L-values and filenames:
     Ls = [40, 60, 80, 100]
     files = [rootdir + f"/data/{L}x{L}-multi.dat" for L in Ls]
-    Tmin, Tmax = 2, 2.3
-    n_temps = 7
-    data = {}
+    Tmin, Tmax = 2, 2.3  # min and max temp.
+    n_temps = 7  # number of temps to simulate per process.
+    data = {}  # dictionary for data.
 
-    n_thrds = mp.cpu_count()//n_temps
+    n_thrds = mp.cpu_count()//n_temps  # number of subprocesses to spawn
 
     def phase_L_sim(i):
-        file = files[i]
+        """Function spawning process and returning it."""
         p = Popen(
             [
                 "./main.exe",
-                file,
+                files[i],
                 "multi",
                 f"{Ls[i]}",
                 f"{nmax}",
@@ -111,28 +112,20 @@ def phase_trans_test(nmax):
         )
         return p
 
-    if n_thrds >= 2:
-        for j in range(len(Ls)-n_thrds+1):
-            print(j)
-            sims = [phase_L_sim(i) for i in range(n_thrds)]
-            [p.wait() for p in sims]
-    else:
-        for L in Ls:
-            file = rootdir + f"/data/{L}x{L}-multi.dat"
-            run(
-                [
-                    "./main.exe",
-                    file,
-                    "multi",
-                    f"{L}",
-                    f"{nmax}",
-                    f"{Tmin}",
-                    f"{Tmax}",
-                    f"{n_temps}"
-                ],
-                cwd=src
-            )
+    # sets number of subprocesses to spawn at the same time:
+    ranges = [len(Ls[i:i+n_thrds]) for i in range(0, len(Ls), n_thrds)]
+    k = 0  # counter for number of subprocesses that have been run
 
+    # iterating through number of subprocesses to spawn at the same time:
+    for i in range(len(ranges)):
+        # spawning subprocesses:
+        sims = [phase_L_sim(j) for j in range(k, ranges[i] + k)]
+        k += len(sims)  # counting up.
+
+        # waiting for all concurrent subprocesses to finish:
+        [p.wait() for p in sims]
+
+    # reading data from files:
     for L, file in zip(Ls, files):
         data[L] = np.genfromtxt(file)
 
@@ -140,55 +133,55 @@ def phase_trans_test(nmax):
 if __name__ == "__main__":
 
     build_cpp()
-    temp = 1
+    # temp = 1
     nmax = int(1e3)
-    L = 2
-    file = rootdir + "/data/test.dat"
-    run(["./main.exe", file, "single", f"{L}", f"{nmax}", f"{temp}"], cwd=src)
-    data = np.genfromtxt(file)
-
-    E = data[:, 0]
-    M = data[:, 1]
-    absM = data[:, 2]
-
-    E_exp = -2*np.sinh(8/temp)/(np.cosh(8/temp) + 3)
-    absM_exp = (2*np.exp(8/temp) + 4)/(np.cosh(8/temp) + 3)/4
-
-    print(f"{np.mean(E)-E_exp:e}")
-    print(f"{np.mean(M):e}")
-    print(f"{np.mean(absM)-absM_exp:e}")
-
-    n_cycles = np.linspace(1, nmax, len(E))
-    E = np.cumsum(E)/n_cycles
-    # M = np.cumsum(M)/n_cycles
-    absM = np.cumsum(absM)/n_cycles
-
-    plt.figure()
-    plt.title(f"Average energy of {L}x{L} lattice with T = {temp}")
-    plt.hlines(E_exp, 0, nmax, 'r', label="Analytic")
-    plt.plot(n_cycles, E, label="<E>")
-    plt.xlabel("N")
-    plt.ylabel("<E>")
-    plt.legend()
-    plt.grid()
-    plt.savefig(
-        rootdir + f"/data/t{temp*10}-{L}x{L}-E.pdf", bbox_inches='tight')
-
-    plt.figure()
-    plt.title(f"Average magnitude of magnetization of {L}x{L}" +
-              f"lattice with T = {temp}")
-    plt.hlines(absM_exp, 0, nmax, 'r', label="Analytic")
-    plt.plot(n_cycles, absM, label="<|M|>")
-    plt.xlabel("N")
-    plt.ylabel("<|M|>")
-    plt.legend()
-    plt.grid()
-    plt.savefig(
-        rootdir + f"/data/t{temp*10}-{L}x{L}-|M|.pdf", bbox_inches='tight')
-
-    stabilization_run(nmax, 1, 20)
-    stabilization_run(nmax, 2.4, 20)
-    stabilization_run(nmax, 1, 20, randspin=True)
-    stabilization_run(nmax, 2.4, 20, randspin=True)
-    # phase_trans_test(nmax)
+    # L = 2
+    # file = rootdir + "/data/test.dat"
+    # run(["./main.exe", file, "single", f"{L}", f"{nmax}", f"{temp}"], cwd=src)
+    # data = np.genfromtxt(file)
+    #
+    # E = data[:, 0]
+    # M = data[:, 1]
+    # absM = data[:, 2]
+    #
+    # E_exp = -2*np.sinh(8/temp)/(np.cosh(8/temp) + 3)
+    # absM_exp = (2*np.exp(8/temp) + 4)/(np.cosh(8/temp) + 3)/4
+    #
+    # print(f"{np.mean(E)-E_exp:e}")
+    # print(f"{np.mean(M):e}")
+    # print(f"{np.mean(absM)-absM_exp:e}")
+    #
+    # n_cycles = np.linspace(1, nmax, len(E))
+    # E = np.cumsum(E)/n_cycles
+    # # M = np.cumsum(M)/n_cycles
+    # absM = np.cumsum(absM)/n_cycles
+    #
+    # plt.figure()
+    # plt.title(f"Average energy of {L}x{L} lattice with T = {temp}")
+    # plt.hlines(E_exp, 0, nmax, 'r', label="Analytic")
+    # plt.plot(n_cycles, E, label="<E>")
+    # plt.xlabel("N")
+    # plt.ylabel("<E>")
+    # plt.legend()
+    # plt.grid()
+    # plt.savefig(
+    #     rootdir + f"/data/t{temp*10}-{L}x{L}-E.pdf", bbox_inches='tight')
+    #
+    # plt.figure()
+    # plt.title(f"Average magnitude of magnetization of {L}x{L}" +
+    #           f"lattice with T = {temp}")
+    # plt.hlines(absM_exp, 0, nmax, 'r', label="Analytic")
+    # plt.plot(n_cycles, absM, label="<|M|>")
+    # plt.xlabel("N")
+    # plt.ylabel("<|M|>")
+    # plt.legend()
+    # plt.grid()
+    # plt.savefig(
+    #     rootdir + f"/data/t{temp*10}-{L}x{L}-|M|.pdf", bbox_inches='tight')
+    #
+    # stabilization_run(nmax, 1, 20)
+    # stabilization_run(nmax, 2.4, 20)
+    # stabilization_run(nmax, 1, 20, randspin=True)
+    # stabilization_run(nmax, 2.4, 20, randspin=True)
+    phase_trans_test(nmax)
     plt.show()
