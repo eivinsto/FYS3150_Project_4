86d7fc409687085a3051a42faae0338254f5751d
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-11-19 17:33:14 +0100

    fixed tolerance in test_functions

--- test_functions.py
+++ test_functions.py
@@ -3,7 +3,6 @@ import project as p
 from subprocess import run
 import numpy as np
 
-tol = 1e-3
 temp = 1  # temperature of system.
 L = 2  # dimensionality of lattice.
 file = p.rootdir + "/data/2x2-test.dat"
@@ -24,7 +23,7 @@ def test_analytic_energy():
 
     # calculating analytic results:
     E_exp = -2*np.sinh(8/temp)/(np.cosh(8/temp) + 3)
-    success = np.isclose(np.mean(E), E_exp, rtol=tol)
+    success = np.isclose(np.mean(E), E_exp, rtol=1e-3)
 
     msg = f"Numeric <E> = {np.mean(E)} != analytic <E> = {E_exp}"
     assert success, msg
@@ -35,7 +34,7 @@ def test_analytic_absM():
 
     # calculating analytic results:
     absM_exp = ((2*np.exp(8/temp) + 4)/(np.cosh(8/temp) + 3))/4
-    success = np.isclose(np.mean(absM), absM_exp, rtol=tol)
+    success = np.isclose(np.mean(absM), absM_exp, rtol=1e-3)
 
     msg = f"Numeric <|M|> = {np.mean(absM)} != analytic <|M|> = {absM_exp}"
     assert success, msg
@@ -47,7 +46,7 @@ def test_analytic_Cv():
     # calculating analytic results:
     Cv = (1/(temp**2)) * (np.mean(E**2) - np.mean(E)**2)
     Cv_exp = (12/(temp**2)) * (np.cosh(8/temp)/((np.cosh(8/temp) + 3)**2))
-    success = np.isclose(Cv, Cv_exp, rtol=tol)
+    success = np.isclose(Cv, Cv_exp, rtol=1e-2)
 
     msg = f"Numeric <Cv> = {Cv} != analytic <Cv> = {Cv_exp}"
     assert success, msg
@@ -59,7 +58,7 @@ def test_analytic_Xi():
     # calculating analytic results:
     Xi = (1/temp) * (np.mean(M**2) - np.mean(M)**2)
     Xi_exp = (1/(2*temp)) * ((np.exp(8/temp) + 1)/(np.cosh(8/temp) + 3))
-    success = np.isclose(Xi, Xi_exp, rtol=tol)
+    success = np.isclose(Xi, Xi_exp, rtol=1e-2)
 
     msg = f"Numeric <Xi> = {Xi} != analytic <Xi> = {Xi_exp}"
     assert success, msg
